87b0647a0a0cd3e62b67ffd715b8072d3c3f47f5
Luke Woodbury <luke@lukewoodbury.co.uk>
2022-02-10 20:55:07 +0000

    Updated Teensy examples
    Fixed teensy code, now working

--- Teensy/Radiohead library/rf69_tx_demo/rf69_tx_demo.ino
+++ Teensy/Radiohead library/rf69_tx_demo/rf69_tx_demo.ino
@@ -14,34 +14,31 @@
 /************ Radio Setup ***************/
 
 // Change to 434.0 or other frequency, must match RX's freq!
-#define RF69_FREQ 433.0
+#define RF69_FREQ 434.0
 
 //For Teensy 3.x and T4.x the following format is required to operate correctly
 //This is a limitation of the RadioHead radio drivers
 #define RFM69_RST     3
 #define RFM69_CS      10
 #define RFM69_INT     digitalPinToInterrupt(2)
-//----- END TEENSY CONFIG
-
-  #define LED           14
 
 // Singleton instance of the radio driver
-RH_RF69 rf69(RFM69_CS, RFM69_INT);
+RH_RF69 rf69(RFM69_CS,RFM69_INT);
+//----- END TEENSY CONFIG
 
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
 void setup() 
 {
   Serial.begin(115200);
-  delay(3000);
   //while (!Serial) { delay(1); } // wait until serial console is open, remove if not tethered to computer
 
   //For Teensy 3.x and T4.x the following format is required to operate correctly
-  pinMode(LED, OUTPUT);     
+  //pinMode(LED, OUTPUT);     
   pinMode(RFM69_RST, OUTPUT);
   digitalWrite(RFM69_RST, LOW);
 
-  Serial.println("Teensy RFM69 RX Test!");
+  Serial.println("Feather RFM69 TX Test!");
   Serial.println();
 
   // manual reset
@@ -56,7 +53,6 @@ void setup()
     while (1);
   }
   Serial.println("RFM69 radio init OK!");
-  
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
   // No encryption
   if (!rf69.setFrequency(RF69_FREQ)) {
@@ -65,48 +61,48 @@ void setup()
 
   // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
   // ishighpowermodule flag set like this:
-  rf69.setTxPower(20);  // range from 14-20 for power, 2nd arg must be true for 69HCW
+  rf69.setTxPower(20,true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
 
   // The encryption key has to be the same as the one in the server
   uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
                     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   rf69.setEncryptionKey(key);
   
-  pinMode(LED, OUTPUT);
+  //pinMode(LED, OUTPUT);
 
   Serial.print("RFM69 radio @");  Serial.print((int)RF69_FREQ);  Serial.println(" MHz");
 }
 
 
-void loop() {
- if (rf69.available()) {
-    // Should be a message for us now   
-    uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-    uint8_t len = sizeof(buf);
-    if (rf69.recv(buf, &len)) {
-      if (!len) return;
-      buf[len] = 0;
-      Serial.print("Received [");
-      Serial.print(len);
-      Serial.print("]: ");
-      Serial.println((char*)buf);
-      Serial.print("RSSI: ");
-      Serial.println(rf69.lastRssi(), DEC);
 
-      if (strstr((char *)buf, "Hello World")) {
-        // Send a reply!
-        uint8_t data[] = "And hello back to you";
-        rf69.send(data, sizeof(data));
-        rf69.waitPacketSent();
-        Serial.println("Sent a reply");
-        Blink(LED, 40, 3); //blink LED 3 times, 40ms between blinks
-      }
+void loop() {
+  delay(1000);  // Wait 1 second between transmits, could also 'sleep' here!
+
+  char radiopacket[20] = "Hello World #";
+  itoa(packetnum++, radiopacket+13, 10);
+  Serial.print("Sending "); Serial.println(radiopacket);
+  
+  // Send a message!
+  rf69.send((uint8_t *)radiopacket, strlen(radiopacket));
+  rf69.waitPacketSent();
+
+  // Now wait for a reply
+  uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
+  uint8_t len = sizeof(buf);
+
+  if (rf69.waitAvailableTimeout(500))  { 
+    // Should be a reply message for us now   
+    if (rf69.recv(buf, &len)) {
+      Serial.print("Got a reply: ");
+      Serial.println((char*)buf);
+      //Blink(LED, 50, 3); //blink LED 3 times, 50ms between blinks
     } else {
       Serial.println("Receive failed");
     }
+  } else {
+    Serial.println("No reply, is another RFM69 listening?");
   }
 }
-
 
 void Blink(byte PIN, byte DELAY_MS, byte loops) {
   for (byte i=0; i<loops; i++)  {
